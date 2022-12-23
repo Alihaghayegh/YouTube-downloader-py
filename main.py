55ddc187ee714786278e6ea3a01d0514617cac2c
ali haghayegh <haghayeghali95@gmail.com>
2022-12-23 21:29:26 +0330

    the main module completed

---
+++ main.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+
+# This module is main function that handles the downloading
+def download(link):
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
+    try:
+        youtube_object.download()
+    except:
+        print("There is a problem in downloading")
+
+    print("All Done...")
+
+
+link = input("Put your link here: ")
+
+if __name__ == "__main__":
+    download(link)
